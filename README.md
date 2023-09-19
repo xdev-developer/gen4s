06ebbcba737d31b79cc28b86e271029e9db9552f
Pavlo Tsipinio <pavel.tsipinio@gmail.com>
2023-09-19 22:02:15 +0300

    added template syntax description to readme

--- README.md
+++ README.md
@@ -60,3 +60,18 @@ output {
 ## Schema definition and data generators
 
 Big thanks to https://github.com/azakordonets/fabricator  random data generator project!
+
+## Template syntax
+
+- \* - generates any symbol
+  - \*{2} - generates random symbols with 2 symbols size
+  - \*{2, 5} - generates random symbols with random size between 2 and 5
+- %w - generates random english word
+  - %w{4} - generates random english word with fixed length. Max available length is 31
+  - %w{2, 6} - generates random english word with random length between 2 and 6
+- %n{2} - returns defined number
+- %n{4, 10} - returns random number between 4 and 10
+- \#{4} - returns random HEX number with provided length (4)
+- \#{4, 8} - returns random HEX number of random length between 4 and 8
+- %ip4, %ip6, %mac - generates random values for IP v4, IP v6 and mac address respectively
+- other values are considered as text tokens
--- generators/src/test/scala/io/gen4s/generators/test/TemplateGeneratorSpec.scala
+++ generators/src/test/scala/io/gen4s/generators/test/TemplateGeneratorSpec.scala
@@ -12,10 +12,10 @@ class TemplateGeneratorSpec extends AsyncWordSpecLike with AsyncIOSpec with Matc
 
   "TemplateGenerator" should {
     "parse template and generate string with random values" in {
-      val template = "We need to send a message to ip %ip4 from %mac with IP v6 %ip6"
+      val template = "We need to send a message to ip '%ip4' from %mac with IP v6 %ip6"
 
       TemplateGenerator.make[IO].process(template).map { res =>
-        res should fullyMatch regex """We need to send a message to ip (?:[0-9]{1,3}\.){3}[0-9]{1,3} from ([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}) with IP v6 (([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"""
+        res should fullyMatch regex """We need to send a message to ip '(?:[0-9]{1,3}\.){3}[0-9]{1,3}' from ([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}) with IP v6 (([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"""
       }
     }
   }
