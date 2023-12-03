0c553fec0388dce3bf88073d305e847f1bb33a32
Den Kovalevskyi <xdev.developer@gmail.com>
2023-12-03 19:49:13 +0100

    Use config file directory as fallback base path for templates and schema

--- app/src/main/scala/io/gen4s/cli/StageConfigLoader.scala
+++ app/src/main/scala/io/gen4s/cli/StageConfigLoader.scala
@@ -2,7 +2,10 @@ package io.gen4s.cli
 
 import java.io.File
 
+import org.apache.commons.io.FilenameUtils
+
 import cats.effect.Sync
+import cats.implicits.*
 import io.gen4s.conf.*
 
 trait StageConfigLoader[F[_]] {
@@ -23,9 +26,30 @@ object StageConfigLoader {
         .file(file)
         .withFallback(ConfigSource.fromConfig(profileConf.value))
         .loadF[F, StageConfig]()
+        .map(resolveRootDirectory)
     }
 
     override def withEmptyEnvProfile(): F[StageConfig] = withEnvProfile(EnvProfileConfig.empty)
+
+    private def resolveRootDirectory(conf: StageConfig): StageConfig = {
+
+      def resolve(dir: String, in: File): File = {
+        if (!in.exists()) {
+          File(dir, FilenameUtils.getName(in.getAbsolutePath))
+        } else in
+      }
+
+      val dir         = FilenameUtils.getFullPath(file.getAbsolutePath)
+      val newSchema   = resolve(dir, conf.input.schema)
+      val newTemplate = resolve(dir, conf.input.template)
+
+      val newRecords = conf.input.csvRecords match {
+        case Some(records) => resolve(dir, records).some
+        case None          => None
+      }
+
+      conf.copy(input = conf.input.copy(schema = newSchema, template = newTemplate, csvRecords = newRecords))
+    }
   }
 
 }
