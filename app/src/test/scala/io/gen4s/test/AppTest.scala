bdda6f9419a10027ac99a31e2ef6573c34613d81
Denys Kovalevskyi <xdev.developer@gmail.com>
2024-02-10 22:56:19 +0100

    App test

---
+++ app/src/test/scala/io/gen4s/test/AppTest.scala
@@ -0,0 +1,44 @@
+package io.gen4s.test
+
+import org.scalatest.freespec.AsyncFreeSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.OptionValues
+
+import cats.effect.{ExitCode, IO}
+import cats.effect.testing.scalatest.AsyncIOSpec
+import io.gen4s.App
+
+class AppTest extends AsyncFreeSpec with AsyncIOSpec with Matchers with OptionValues {
+
+  "App" - {
+    "Run" in {
+      App.run(List("run", "-c", "./examples/playground/config.conf")).asserting { code =>
+        code shouldBe ExitCode.Success
+      }
+    }
+
+    "Preview" in {
+      App.run(List("preview", "-c", "./examples/playground/config.conf")).asserting { code =>
+        code shouldBe ExitCode.Success
+      }
+    }
+
+    "Run scenario" in {
+      App.run(List("scenario", "-c", "./examples/scenario/scenario.conf")).asserting { code =>
+        code shouldBe ExitCode.Success
+      }
+    }
+
+    "Fail without config" in {
+      App.run(List("run")).asserting { code =>
+        code shouldBe ExitCode.Error
+      }
+    }
+
+    "Show help" in {
+      App.run(List("--help")).asserting { code =>
+        code shouldBe ExitCode.Success
+      }
+    }
+  }
+}
