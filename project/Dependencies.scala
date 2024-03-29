3268a1feda6325177215cf1bafb177cbd922862b
Scala Steward <scala_steward@virtuslab.com>
2024-03-29 02:01:29 +0000

    Update fs2-kafka, fs2-kafka-vulcan to 3.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val catsRetry         = "3.1.0"
     val circe             = "0.14.6"
     val fs2               = "3.10.1"
-    val fs2Kafka          = "3.3.1"
+    val fs2Kafka          = "3.4.0"
     val log4cats          = "2.6.0"
     val pureConfig        = "0.17.6"
     val sttp              = "3.9.5"
