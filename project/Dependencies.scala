078b64cc027ee5811171bc45bf9789c93ae49cf4
Scala Steward <scala_steward@virtuslab.com>
2024-05-22 04:48:47 +0000

    Update client3:async-http-client-backend-cats, ... to 3.9.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val fs2Kafka          = "3.5.1"
     val log4cats          = "2.7.0"
     val pureConfig        = "0.17.6"
-    val sttp              = "3.9.6"
+    val sttp              = "3.9.7"
     var refined           = "0.11.1"
     val parserCombinators = "2.4.0"
     val csv               = "1.3.10"
