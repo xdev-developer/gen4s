b256cb86dca66055a7fd697b630191c641047a9b
Scala Steward <scala_steward@virtuslab.com>
2024-07-30 11:41:14 +0000

    Update protobuf-java-util to 3.25.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -130,7 +130,7 @@ object Dependencies {
 
   val ProtoConverter: Seq[ModuleID] = List(
     "io.confluent"        % "kafka-protobuf-serializer" % "7.6.1",
-    "com.google.protobuf" % "protobuf-java-util"        % "3.25.3"
+    "com.google.protobuf" % "protobuf-java-util"        % "3.25.4"
   )
 
   val ScalaTest: Seq[ModuleID]      = List("org.scalatest" %% "scalatest" % V.scalaTest % Test)
