f41471865bb9abf4a08f78a43508ea3a4e4ccc4f
Scala Steward <scala_steward@virtuslab.com>
2024-08-22 03:18:02 +0000

    Update scala-csv to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val sttp              = "3.9.8"
     var refined           = "0.11.2"
     val parserCombinators = "2.4.0"
-    val csv               = "1.4.1"
+    val csv               = "2.0.0"
     val monocle           = "3.3.0"
 
     val scalaTest      = "3.2.19"
