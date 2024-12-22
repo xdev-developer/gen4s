d0f5c5ae2a6f204f273f3f19f71265aa11fc1f43
Scala Steward <scala_steward@virtuslab.com>
2024-12-22 02:43:49 +0000

    Update logback-classic to 1.5.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
 
     val betterMonadicFor = "0.3.1"
     val kindProjector    = "0.13.2"
-    val logback          = "1.5.14"
+    val logback          = "1.5.15"
   }
 
   val Cats: Seq[ModuleID] = List(
