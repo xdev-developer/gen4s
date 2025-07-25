790886f88e463058b9bd5b32c3abf5786d00b4ec
Scala Steward <scala_steward@virtuslab.com>
2025-07-25 22:24:33 +0000

    Update commons-text to 1.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,11 +39,11 @@ object Dependencies {
   )
 
   val ApacheCommonsText: Seq[ModuleID] = List(
-    "org.apache.commons" % "commons-text" % "1.13.1"
+    "org.apache.commons" % "commons-text" % "1.14.0"
   )
 
   val ApacheCommons: Seq[ModuleID] = List(
-    "org.apache.commons" % "commons-text"  % "1.13.1",
+    "org.apache.commons" % "commons-text"  % "1.14.0",
     "commons-codec"      % "commons-codec" % "1.18.0",
     "org.xerial.snappy"  % "snappy-java"   % "1.1.10.8",
     "commons-io"         % "commons-io"    % "2.20.0"
