db28577bafdcc267874e9a5c74e7449896f5a5cd
Scala Steward <scala_steward@virtuslab.com>
2025-11-29 01:26:59 +0000

    Update kafka-protobuf-serializer to 7.9.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -136,7 +136,7 @@ object Dependencies {
   )
 
   val ProtoConverter: Seq[ModuleID] = List(
-    "io.confluent"        % "kafka-protobuf-serializer" % "7.9.2",
+    "io.confluent"        % "kafka-protobuf-serializer" % "7.9.5",
     "com.google.protobuf" % "protobuf-java-util"        % "3.25.8"
   )
 
